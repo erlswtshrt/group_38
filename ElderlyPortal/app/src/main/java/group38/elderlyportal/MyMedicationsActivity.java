93847b90a0e24dad6b78df0726f85d79d788a48c
scob <scob@seas.upenn.edu>
2015-03-26 22:41:15 -0400

    medicine list has sorting

--- ElderlyPortal/app/src/main/java/group38/elderlyportal/MyMedicationsActivity.java
+++ ElderlyPortal/app/src/main/java/group38/elderlyportal/MyMedicationsActivity.java
@@ -1,3 +1,4 @@
+
 package group38.elderlyportal;
 
 import android.app.Activity;
@@ -6,6 +7,7 @@ import android.os.Bundle;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,115 +19,21 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.view.*;
 import android.widget.*;
 import java.util.* ;
-
-
-/**
- * Created by JohnEarle on 3/21/15.
- */
-//public class MyMedicationsActivity extends ListActivity {
-
-    /* OLD STUFF
-    private MyMedicationsView view;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        view = new MyMedicationsView(this);
-        setContentView(view);
-    }
-    */
-
-    /*
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-        final ArrayList<Medicine> list = new ArrayList<Medicine>();
-        Medicine med1 = new Medicine ("medicine 1", new GregorianCalendar(), "Monday") ;
-        Medicine med2 = new Medicine ("medicine 2", new GregorianCalendar(), "Tuesday") ;
-        Medicine med3 = new Medicine ("medicine 3", new GregorianCalendar(), "Wednesday") ;
-        list.add (med1) ;
-        list.add (med2) ;
-        list.add (med2) ;
-
-        // use your custom layout
-        MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this,
-                R.layout.medicine_list_row, R.id.label, list);
-        setListAdapter(adapter);
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        String item = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
-    }
-
-    private class StableArrayAdapter extends ArrayAdapter<Medicine> {
-
-        HashMap<Medicine, Integer> mIdMap = new HashMap<Medicine, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<Medicine> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            Medicine item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-    public class MySimpleArrayAdapter extends ArrayAdapter<Medicine> {
-        private final Context context;
-        private final Medicine[] values;
-
-        public MySimpleArrayAdapter(Context context, Medicine[] values) {
-            super(context, R.layout.medicine_list_row, values);
-            this.context = context;
-            this.values = values;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = (LayoutInflater) context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View rowView = inflater.inflate(R.layout.medicine_list_row, parent, false);
-            TextView textView = (TextView) rowView.findViewById(R.id.label);
-            ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-            textView.setText(values[position].getName());
-            // change the icon for Windows and iPhone
-            Medicine s = values[position];
-            if (s.getName().startsWith("iPhone")) {
-                imageView.setImageResource(R.drawable.ic_launcher);
-            } else {
-                imageView.setImageResource(R.drawable.ic_launcher);
-            }
-
-            return rowView;
-        }
-    }
-
-}
-*/
-
+/*
 public class MyMedicationsActivity extends ListActivity {
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
 
-        Medicine med1 = new Medicine ("medicine 1", new GregorianCalendar(), "Monday") ;
-        Medicine med2 = new Medicine ("medicine 2", new GregorianCalendar(), "Tuesday") ;
-        Medicine med3 = new Medicine ("medicine 3", new GregorianCalendar(), "Wednesday") ;
+    MySimpleArrayAdapter adapter ;
+
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        //setContentView (R.layout.activity_my_medications) ;
+
+        Medicine med1 = new Medicine ("medicine 2", new GregorianCalendar(), "Monday") ;
+        Medicine med2 = new Medicine ("medicine 3", new GregorianCalendar(), "Tuesday") ;
+        Medicine med3 = new Medicine ("medicine 4", new GregorianCalendar(), "Wednesday") ;
 
         Medicine[] list = new Medicine[] {med1, med2, med3} ;
-        MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, list);
+        adapter = new MySimpleArrayAdapter(this, list);
         setListAdapter(adapter);
     }
 
@@ -133,6 +41,26 @@ public class MyMedicationsActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
         Medicine item = (Medicine) getListAdapter().getItem(position);
         Toast.makeText(this, item.getName() + " selected", Toast.LENGTH_LONG).show();
+    }
+
+    public void sort1 () {
+        Toast.makeText (this, "Sort1", Toast.LENGTH_LONG).show();
+        adapter.sort(new Comparator<Medicine>() {
+            @Override
+            public int compare(Medicine m1, Medicine m2) {
+                return m1.getName().compareTo(m2.getName());
+            }
+        });
+    }
+
+    public void sort2 () {
+        Toast.makeText (this, "Sort2", Toast.LENGTH_LONG).show();
+        adapter.sort(new Comparator<Medicine>() {
+            @Override
+            public int compare(Medicine m1, Medicine m2) {
+                return m1.getDateOfNextRefill().compareTo(m2.getDateOfNextRefill());
+            }
+        });
     }
 
     private class MySimpleArrayAdapter extends ArrayAdapter<Medicine> {
@@ -150,17 +78,163 @@ public class MyMedicationsActivity extends ListActivity {
             LayoutInflater inflater = (LayoutInflater) context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View rowView = inflater.inflate(R.layout.medicine_list_row, parent, false);
-            TextView textView = (TextView) rowView.findViewById(R.id.label);
+            TextView textView1 = (TextView) rowView.findViewById(R.id.label1);
+            TextView textView2 = (TextView) rowView.findViewById(R.id.label2);
             ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-            textView.setText(values[position].getName());
-            // Change the icon for Windows and iPhone
+            textView1.setText(values[position].getName());
+            textView2.setText(values[position].getDosage());
+
             String s = values[position].getName();
-            if (s.startsWith("Windows7") || s.startsWith("iPhone")
-                    || s.startsWith("Solaris")) {
-                imageView.setImageResource(R.drawable.ic_launcher);
-            } else {
-                imageView.setImageResource(R.drawable.ic_launcher);
+            imageView.setImageResource(R.drawable.ic_launcher);
+
+            return rowView;
+        }
+    }
+}
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.app.ListActivity;
+public class MyMedicationsActivity extends ListActivity {
+    private TextView text;
+    private List<String> listValues;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+
+        text = (TextView) findViewById(R.id.mainText);
+
+        Medicine med1 = new Medicine ("medicine 1", new GregorianCalendar (2015,3,26,3,1), "1 dose") ;
+        Medicine med2 = new Medicine ("medicine 2", new GregorianCalendar (2015,3,26,5,0), "1 dose") ;
+        Medicine med3 = new Medicine ("medicine 3", new GregorianCalendar (2015,9,2,12,47), "1 dose") ;
+        Medicine med4 = new Medicine ("medicine 4", new GregorianCalendar (2015,3,28,8,9), "1 dose") ;
+        Medicine med5 = new Medicine ("medicine 5", new GregorianCalendar (2015,12,28,2,26), "1 dose") ;
+        Medicine med6 = new Medicine ("medicine 6", new GregorianCalendar (2015,3,27,7,47), "1 dose") ;
+        Medicine med7 = new Medicine ("medicine 7", new GregorianCalendar (2014,3,28,8,9), "1 dose") ;
+        Medicine med8 = new Medicine ("medicine 8", new GregorianCalendar (2015,3,28,11,26), "1 dose") ;
+
+        Medicine[] list = new Medicine[] {med1, med2, med3, med4, med5, med6, med7, med8} ;
+
+        // initiate the listadapter
+        MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, list);
+
+        // assign the list adapter
+        setListAdapter(adapter);
+
+        addListenerOnSpinnerItemSelection () ;
+    }
+
+    private String formatGregorianCalendar (GregorianCalendar gc) {
+        int year = gc.get(GregorianCalendar.YEAR) ;
+        int month = gc.get(GregorianCalendar.MONTH) ;
+        int day = gc.get(GregorianCalendar.DAY_OF_MONTH) ;
+        int hour = gc.get(GregorianCalendar.HOUR_OF_DAY) ;
+        int minute = gc.get(GregorianCalendar.MINUTE) ;
+        return String.format("%02d",month) + "/" +
+               String.format("%02d",day) + "/" +
+               String.format("%04d",year) + " at " +
+               String.format("%02d",hour) + ":" +
+               String.format("%02d",minute) ;
+    }
+
+    private void addListenerOnSpinnerItemSelection () {
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner) ;
+        spinner.setOnItemSelectedListener (new SpinnerListener (this, spinner)) ;
+    }
+
+    private class SpinnerListener implements AdapterView.OnItemSelectedListener {
+
+        private MyMedicationsActivity activity ;
+        private Spinner spinner ;
+
+        public SpinnerListener (MyMedicationsActivity activity, Spinner spinner) {
+            super () ;
+            this.activity = activity ;
+            this.spinner = spinner ;
+        }
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String selected = spinner.getSelectedItem().toString() ;
+            Log.v (this.toString(), selected) ;
+            if (selected.equals("Name")) {
+                activity.sort1 (spinner) ;
             }
+            else if (selected.equals("Date")) {
+                activity.sort2 (spinner) ;
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
+    // when an item of the list is clicked
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id) {
+        super.onListItemClick(list, view, position, id);
+
+        Medicine selectedItem = (Medicine) getListView().getItemAtPosition(position);
+        //String selectedItem = (String) getListAdapter().getItem(position);
+
+        text.setText("You clicked " + selectedItem.getName() + " at position " + position);
+    }
+
+    public void sort1 (View view) {
+        Log.v(this.toString(), "SORT1") ;
+        MySimpleArrayAdapter adapter = (MySimpleArrayAdapter) this.getListAdapter() ;
+        adapter.sort(new Comparator<Medicine>() {
+            @Override
+            public int compare(Medicine m1, Medicine m2) {
+                return m1.getName().compareTo(m2.getName());
+            }
+        });
+    }
+
+    public void sort2 (View view) {
+        Log.v(this.toString(), "SORT2") ;
+        MySimpleArrayAdapter adapter = (MySimpleArrayAdapter) this.getListAdapter() ;
+        adapter.sort(new Comparator<Medicine>() {
+            @Override
+            public int compare(Medicine m1, Medicine m2) {
+                return m1.getDateOfNextRefill().compareTo(m2.getDateOfNextRefill());
+            }
+        });
+    }
+
+    private class MySimpleArrayAdapter extends ArrayAdapter<Medicine> {
+        private final Context context;
+        private final Medicine[] values;
+
+        public MySimpleArrayAdapter(Context context, Medicine[] values) {
+            super(context, R.layout.medicine_list_row, values);
+            this.context = context;
+            this.values = values;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.medicine_list_row, parent, false);
+            TextView textView1 = (TextView) rowView.findViewById(R.id.label1);
+            TextView textView2 = (TextView) rowView.findViewById(R.id.label2);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+            textView1.setText(values[position].getName());
+            textView2.setText(formatGregorianCalendar(values[position].getDateOfNextRefill()));
+
+            String s = values[position].getName();
+            imageView.setImageResource(R.drawable.ic_launcher);
 
             return rowView;
         }
