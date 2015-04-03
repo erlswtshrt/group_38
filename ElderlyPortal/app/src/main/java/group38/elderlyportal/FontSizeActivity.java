d85e4713b7a506bafe2de53b5fda68c56392546b
weineil <neilwei@seas.upenn.edu>
2015-04-03 04:29:22 -0400

    added styles and FontSizeActivity class to handle text size changes

---
+++ ElderlyPortal/app/src/main/java/group38/elderlyportal/FontSizeActivity.java
@@ -0,0 +1,13 @@
+package group38.elderlyportal;
+
+import android.app.Activity;
+
+/**
+ * Created by Neil on 4/3/2015.
+ */
+public class FontSizeActivity extends Activity {
+    @Override
+    public void onStart() {
+        
+    }
+}
